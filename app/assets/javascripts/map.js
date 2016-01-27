d9eb6e87a0a18f9ff7edc0d5834131573ed66cae
LeShoyaStarkey <olstarkey@gmail.com>
2016-01-26 17:07:48 -0800

    added search tool

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,10 +1,5 @@
 var map;
 
-
-
-
-
-
 $(function() {
 
   if (navigator === undefined) {
@@ -16,24 +11,28 @@ $(function() {
       initializeMap(lat, lng);
     });
   }
-
   var initializeMap = function(lat, lng) {
     var mapDiv = $("#map")[0];
-
     var map = new google.maps.Map(mapDiv, {
       center: {lat: lat, lng: lng},
       zoom: 13
     });
-  };
 
-    geocoder = new google.maps.Geocoder();
-    geocoder.geocode({'address': "New York City"}, function(results, status) {
-    var lat = results[0].geometry.location.lat();
-    var lng = results[0].geometry.location.lng();
-    // Do things with lat/lng here
-  });
+    var geocoder = new google.maps.Geocoder();
 
+    $("#submit").click(function(){
+      var address = $("#address").val();
+        // need address variable each time for the click function to work
+        geocoder.geocode({'address': address }, function(results) {
+          console.log(results);
+          var lat = results[0].geometry.location.lat();
+          var lng = results[0].geometry.location.lng();
+          // Do things with lat/lng here
+          map.setCenter({lat: lat, lng:lng})
+        // .setCenter will move the map
+        });
+      });
 
+    };
 
-
-});
+ });

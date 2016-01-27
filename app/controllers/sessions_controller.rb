879bc3ab01681bb450524ede477f69eb9a3080c1
LeShoyaStarkey <olstarkey@gmail.com>
2016-01-27 10:56:51 -0800

    corrected signlink error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-     
-   else
-     render "new"
+    
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,4 @@ class UsersController < ApplicationController
 
 
 
-  private
-
-   def user_params
-     params.require(:user).permit(:username, :email, :password, :password_confirmation)
-   end
-
 end

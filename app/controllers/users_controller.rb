ac6d00518c07efc266603091b8ef35122cf2b7fa
LeShoyaStarkey <olstarkey@gmail.com>
2016-01-27 10:53:42 -0800

    added working signin/signup links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,12 @@ class UsersController < ApplicationController
     end
   end
 
+
+
+  private
+
+   def user_params
+     params.require(:user).permit(:username, :email, :password, :password_confirmation)
+   end
+
 end

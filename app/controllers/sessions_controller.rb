9d891a0bbdb6316e3133f480a547722dcf9ffc37
LeShoyaStarkey <olstarkey@gmail.com>
2016-01-27 11:09:11 -0800

    created sign-in route and controller actions(get and post)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
-  end
+    user = User.find_by(email: params[:session][:email])
+     if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+       redirect_to root_path
+     else
+    render "new"
+   end
+ end
 
   def destroy
 
   end
+
 end

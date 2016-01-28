cf4ec263436fc0a572b6fb82ff75ee7316ebf231
LeShoyaStarkey <olstarkey@gmail.com>
2016-01-28 14:58:08 -0800

    added zillow deep comps api data

--- app/controllers/zillow_test_controller.rb
+++ app/controllers/zillow_test_controller.rb
@@ -4,12 +4,9 @@ require "rubillow"
 
     def zillow
       @property = Rubillow::PropertyDetails.deep_search_results({ :address => '601 1st St NW', :citystatezip => 'moultrie,ga,31768' })
-      # @prop_details= Rubillow::PropertyDetails.updated_property_details({:zpid => "49076278"})
       @propertyz = @property.zpid
       @property_comps = Rubillow::PropertyDetails.deep_comps({ :zpid => @propertyz , :count => 5 })
-
       @property_updated = Rubillow::PropertyDetails.updated_property_details({zpid: @propertyz})
-
     end
 
   end

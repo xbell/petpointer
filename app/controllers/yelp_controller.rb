79383be0ef29c81b2917d87f66cdcd8a8302fbb6
danieladler <weston.adler@gmail.com>
2016-01-28 11:53:52 -0800

    fix styling and update html of search bar

--- app/controllers/yelp_controller.rb
+++ app/controllers/yelp_controller.rb
@@ -7,7 +7,7 @@ class YelpController < ApplicationController
     # don't think we need this once searches come straight from homepage and/or map page
     @response = Yelp.client.search("1410 NE Campus Parkway, Seattle WA 98195", { term: "veterinary", limit: 20, sort: 1, radius_filter: 8046.72})
   end
-  
+
   def search
     @address  = params[:address]
     @term     = params[:term]

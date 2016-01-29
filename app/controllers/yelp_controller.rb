a32c653aee794f58adaf5f7b41dd03fb95eb7b2e
danieladler <weston.adler@gmail.com>
2016-01-28 16:25:24 -0800

    add category filters and 10 results/term => 40 total results

--- app/controllers/yelp_controller.rb
+++ app/controllers/yelp_controller.rb
@@ -9,11 +9,13 @@ class YelpController < ApplicationController
   end
 
   def search
-    @address  = params[:address]
-    @term     = params[:term]
-    @response = Yelp.client.search(@address, { term: @term, limit: 20, sort: 1, radius_filter: 8046.72})
-    @response = @response.businesses.to_json
-    render json: @response
+    @address      = params[:address]
+    @vets         = Yelp.client.search(@address, {category_filter: 'vet', limit: 10, sort: 1, radius_filter: 8046.72})
+    @pet_services = Yelp.client.search(@address, {category_filter: 'dogwalkers,pet_sitting,groomer,pet_training', limit: 10, sort: 1, radius_filter: 8046.72})
+    @pet_stores   = Yelp.client.search(@address, {category_filter: 'petstore', limit: 10, sort: 1, radius_filter: 8046.72})
+    @parks        = Yelp.client.search(@address, {category_filter: 'parks,dog_parks,beaches', limit: 10, sort: 1, radius_filter: 8046.72})
+    @response     = @vets.businesses + @pet_services.businesses + @pet_stores.businesses + @parks.businesses
+    render json: @response.to_json
   end
 
 end

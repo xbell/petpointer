72d57a87ad31697bc14a40f194d3f55468b44399
Evelyn <evieb16@gmail.com>
2016-01-29 15:26:59 -0800

    Fixes merge conflict

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,10 +1,7 @@
 class MapController < ApplicationController
 
   def index
-<<<<<<< HEAD
     # @response = Yelp.client.search("1410 NE Campus Parkway, Seattle WA 98195")
-=======
->>>>>>> 8f4635505c7200d0895045de2891cb618d156763
   end
 
 end

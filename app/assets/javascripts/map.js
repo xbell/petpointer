23754c94c8063dd825797efc7ea314b36b44042a
Evelyn <evieb16@gmail.com>
2016-02-04 12:19:54 -0800

    Keeps star checked if zillow_id is in database

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -6,13 +6,14 @@ var geocoder;
 $(function() {
   // saves and deletes favorites but doesn't remember if address has been saved yet
   // need to replace address with Zillow Id
-  $('#rating-input-1-1').on('change', function() {
+  $("#rating-input-1-1").on("change", function() {
     if ($(this).is(':checked')) {
       // get current marker position
       geocoder.geocode({"location": marker.position}, function(response) {
         response[0].formatted_address;
         // ajax request to my server to save address into database
-        $.post("/map", {address: response[0].formatted_address});
+        // $.post("/map", {zillow_id: response[0].formatted_address});
+        $.post("/map", {zillow_id: "48712906"});
       });
     } else {
       geocoder.geocode({"location": marker.position}, function(response) {
@@ -20,13 +21,14 @@ $(function() {
         $.ajax({
           url: "/map",
           type: "DELETE",
-          data: {address: response[0].formatted_address}
+          // data: {zillow_id: response[0].formatted_address}
+          data: {zillow_id: "48712906"}
         });
       });
     }
   });
 
-  $('.expander-trigger').click(function(){
+  $('.expander-trigger').click(function() {
     $(this).toggleClass("expander-hidden");
   });
   // javascript for expander
@@ -75,7 +77,7 @@ $(function() {
 
   geocoder = new google.maps.Geocoder();
 
-  $("#map-search").submit(function(event){
+  $("#map-search").submit(function(event) {
     event.preventDefault();
     var address = $("#address").val();
 
@@ -92,6 +94,22 @@ $(function() {
       // AJAX
       $.get("/yelp", function(data) {
         $("#ajax-response").html("My shizz ipsizzle pimpin' fizzle amet, shizzle my nizzle crocodizzle adipiscing elit. Nullam sapizzle velit, izzle volutpizzle, suscipit quizzle, gravida vel, dang. Pellentesque you son of a bizzle fo shizzle. Break yo neck, yall erizzle. Phat izzle dolor dapibizzle fo tempus fo shizzle. Maurizzle pellentesque nibh fo shizzle crunk. That's the shizzle izzle fo shizzle my nizzle. Pellentesque funky fresh crazy pot. In ass that's the shizzle platea dictumst. Donec sheezy. Fo shizzle mah nizzle fo rizzle, mah home g-dizzle bizzle urna, pretium the bizzle, ghetto ac, eleifend daahng dawg, nunc. Fo shizzle suscipit. Fo shizzle my nizzle mammasay mammasa mamma oo sa velit sizzle purus.");
+      });
+      // keeps star checked or not
+      $.get("/favorites/48712906", function(data, status) {
+
+        console.log(status);
+        // if 200 then true
+        if(status === "success") {
+          // alert("success");
+          $("#rating-input-1-1").prop("checked", true);
+          // $("#rating-input-1-1").html("checked");
+        // if 404 then false
+        } else {
+          // alert("fail");
+          $("#rating-input-1-1").prop("checked", false);
+          // $("#rating-input-1-1").html("unchecked");
+        }
       });
     });
 
